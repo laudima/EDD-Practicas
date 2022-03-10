c854c6692c18f71de0c401c9cafad20365711b34
Laura Itzel Rodríguez Dimayuga <laudima@ciencias.unam.mx>
2022-03-10 15:03:21 -0600

    archivos

---
+++ edd/src/Practica1.java
@@ -0,0 +1,259 @@
+package Clases;
+
+import java.util.Iterator;
+
+public class Practica1 {
+
+    //hola
+
+    public static Lista<Integer> AgregaOrdenado(Lista<Integer> lista, int nuevo) {
+        //Tu codigo aqui
+        if (lista.isEmpty()){
+            lista.add(nuevo);
+        }else{
+            //lista.insert(i, elemento)
+            IteradorLista<Integer> i = lista.iteradorLista();
+            i.start();
+            int index_tracker = 0;
+            while(i.hasNext()){
+                int index =(int) i.next();
+                if(index <= nuevo){
+                    index_tracker++;
+                }else{
+                    break;
+                }
+            }
+            if(index_tracker == 0){
+                lista.agregaInicio(nuevo);
+            }else if(index_tracker >= lista.size()){
+                lista.agregaFinal(nuevo);
+            }else{
+                lista.insert(index_tracker-1, nuevo);
+            }
+        }
+        return lista;
+    }
+
+    // Aqui va tu comentario
+    public static void Union(Lista<Integer> lista1,Lista<Integer> lista2) {
+        IteradorLista<Integer> i = lista1.iteradorLista();
+        IteradorLista<Integer> j = lista2.iteradorLista();
+        i.start(); //Arrrrrancaaaan
+        while (i.hasNext()){ //n repeticiones
+            Boolean repetido = false;
+            int index = (int) i.next();
+            j.start();
+            while(j.hasNext()){ //m repeticiones en cada n-vuelta
+                int jindex = (int) j.next(); //jeje jindex
+                if (index == jindex){
+                    repetido = true;
+                    break;
+                }
+            }
+            if (!repetido){
+                lista2.add(index);
+            }
+        }
+        return;
+    }
+
+    // Aqui va tu comentario
+    public static void Interseccion(Lista<Integer> lista,Lista<Integer> lista2) {
+        IteradorLista<Integer> i = lista.iteradorLista();
+        IteradorLista<Integer> j = lista2.iteradorLista();
+        i.start(); //Arrrrrancaaaan
+        while (i.hasNext()){ //n repeticiones
+            Boolean repetido = false;
+            int index = (int) i.next();
+            j.start();
+            while(j.hasNext()){ //m repeticiones en cada n-vuelta
+                int jindex = (int) j.next(); //jeje jindex
+                if (index == jindex){
+                    repetido = true;
+                    break;
+                }
+            }
+            if (!repetido){
+                lista.delete(index);
+            }
+        }
+        return;
+    }
+
+
+
+    public static void main(String[] args) {
+        Lista<Integer> primera = new Lista<Integer>();
+        Lista<Integer> segunda = new Lista<Integer>();
+        Lista<Integer> tercera = new Lista<Integer>();
+
+
+        // Tests toString
+        for (int i = 0; i <= 5; i++) {
+            primera.add(i);
+        }
+
+        String test = "0 -> 1 -> 2 -> 3 -> 4 -> 5";
+        if (!primera.toString().equals(test)) {
+            System.out.println("1 El toString no funciona!");
+        }
+        primera = new Lista<Integer>();
+        if (!primera.toString().equals("")) {
+            System.out.println("2 El toString no funciona!");
+        }
+
+        // Tests Reverse
+        primera = new Lista<Integer>();
+        segunda = new Lista<Integer>();
+
+        for (int i = 0; i <= 10; i++) {
+            primera.add(i);
+            segunda.agregaInicio(i);
+        }
+
+        primera.reverse();
+        if (!primera.toString().equals(segunda.toString())) {
+            System.out.println("1 El reverse no funciona!");
+        }
+        primera = new Lista<Integer>();
+        segunda = new Lista<Integer>();
+        primera.reverse();
+        if (!primera.toString().equals(segunda.toString())) {
+            System.out.println("2 El reverse no funciona!");
+        }
+
+        // Tests Append
+        primera = new Lista<Integer>();
+        segunda = new Lista<Integer>();
+        for (int i = 0; i <= 10; i++) {
+            primera.add(i);
+            segunda.add(i);
+        }
+        for (int i = 0; i <= 10; i++) {
+            segunda.add(i);
+        }
+        primera.append(primera.clone());
+
+
+        if (!primera.toString().equals(segunda.toString())) {
+            System.out.println("1 El Append no funciona!");
+        }
+
+        // Tests IndexOf
+        if (primera.indexOf(0) != 0) {
+            System.out.println("1 El IndexOf no funciona!");
+        }
+        if (primera.indexOf(1) != 1) {
+            System.out.println("2 El IndexOf no funciona!");
+        }
+        if (primera.indexOf(10) != 10) {
+            System.out.println("3 El IndexOf no funciona!");
+        }
+
+        // Tests Insert
+        primera = new Lista<Integer>();
+        segunda = new Lista<Integer>();
+        for (int i = 0; i <= 10; i++) {
+            primera.add(i);
+
+        }
+        for (int i = 0; i <= 4; i++) {
+            segunda.add(i);
+
+        }
+        segunda.add(6);
+        for (int i = 5; i <= 10; i++) {
+            segunda.add(i);
+
+        }
+
+        primera.insert(5, 6);
+        if (!primera.toString().equals(segunda.toString())) {
+            System.out.println("1 El insert no funciona!");
+        }
+
+        // Tests Mezcla Alternada
+        primera = new Lista<Integer>();
+        segunda = new Lista<Integer>();
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                primera.add(i);
+            }
+        }
+        primera.add(11);
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 != 0) {
+                segunda.add(i);
+            }
+
+        }
+        for (int i = 0; i <= 11; i++) {
+
+                tercera.add(i);
+
+        }
+
+
+        primera.mezclaAlternada(segunda);
+        if (!primera.toString().equals(tercera.toString())) {
+            System.out.println("1 la mezclaAlternada no funciona!");
+        }
+
+
+        // Tests Agrega Ordenado
+        primera = new Lista<Integer>();
+        segunda = new Lista<Integer>();
+        for (int i = 0; i <= 10; i++) {
+            primera.add(i);
+        }
+        for (int i = 0; i <= 9; i++) {
+            segunda.add(i);
+        }
+        segunda.add(9);
+        segunda.add(10);
+
+
+        tercera = AgregaOrdenado(primera,9);
+        if (!tercera.toString().equals(segunda.toString())) {
+            System.out.println("1 el agregaOrdenado no funciona!");
+        }
+
+        // Tests Union
+        primera = new Lista<Integer>();
+        segunda = new Lista<Integer>();
+        tercera = new Lista<Integer>();
+        primera.add(1);
+        primera.add(2);
+        primera.add(3);
+        segunda.add(2);
+        Union(primera, segunda);
+
+        if (!(primera.contains(1) && primera.contains(2) && primera.contains(3) && primera.size() == 3)) {
+            System.out.println("1 La union no funciona!");
+        }
+
+        // Tests interseccion
+        primera = new Lista<Integer>();
+        segunda = new Lista<Integer>();
+        tercera = new Lista<Integer>();
+        primera.add(1);
+        primera.add(2);
+        primera.add(3);
+        segunda.add(2);
+        Interseccion(primera, segunda);
+
+        if (!(primera.contains(2) && primera.size() == 1)) {
+            System.out.println("1 La intersección no funciona!");
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
